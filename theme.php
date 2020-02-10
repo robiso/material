40b20c758f98158e1b7ea7499ae436938bbe43bd
Robert Isoski <r@robiso.com>
2020-02-10 20:08:28 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -11,8 +11,8 @@
         <meta name="description" content="<?= $Wcms->page('description') ?>">
         <meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
-        <?= $Wcms->css() ?>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<?= $Wcms->css() ?>
         <link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">    
     </head>
 
